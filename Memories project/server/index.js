1dfd552d983f460d1e4209b7d2886b3ad06747e5
Parthasarathi Singh <pssingh050205@gmail.com>
2024-03-14 12:13:54 +0530

    changed

--- Memories project/server/index.js
+++ Memories project/server/index.js
@@ -13,8 +13,6 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
-const CONNECTION_URL = 'mongodb+srv://PARTHASARATHI_SINGH:partha103@cluster0.xqcuvbl.mongodb.net';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })

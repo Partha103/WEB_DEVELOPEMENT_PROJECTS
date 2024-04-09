f364c54186897d7c92b45ebc9edb6d6a26be4f34
Parthasarathi Singh <pssingh050205@gmail.com>
2024-04-09 22:32:14 +0530

    Updated with data

--- Major Project/models/listing.js
+++ Major Project/models/listing.js
@@ -1,3 +1,26 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const listingSchema = new Schema({
+//     title: {
+//         type: String,
+//         required: true,
+//     },
+//     description: String,
+//     image: {
+//         type: String,
+//         default: "https://www.istockphoto.com/signature/photo/modern-luxury-house-with-private-infinity-pool-in-dusk-gm1319453433-406317068",
+//         set: (v) => v === ""? "https://www.istockphoto.com/signature/photo/modern-luxury-house-with-private-infinity-pool-in-dusk-gm1319453433-406317068" : v,
+//     },
+//     price: Number,
+//     location: String,
+//     country: String,
+// });
+
+// const Listing = mongoose.model("Listing", listingSchema);
+// module.exports = Listing;
+
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -10,7 +33,7 @@ const listingSchema = new Schema({
     image: {
         type: String,
         default: "https://www.istockphoto.com/signature/photo/modern-luxury-house-with-private-infinity-pool-in-dusk-gm1319453433-406317068",
-        set: (v) => v === ""? "https://www.istockphoto.com/signature/photo/modern-luxury-house-with-private-infinity-pool-in-dusk-gm1319453433-406317068" : v,
+        set: (v) => typeof v === 'string' && v !== "" ? v : "https://www.istockphoto.com/signature/photo/modern-luxury-house-with-private-infinity-pool-in-dusk-gm1319453433-406317068",
     },
     price: Number,
     location: String,

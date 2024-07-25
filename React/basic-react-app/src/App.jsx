40b4ed224f8f33f540ec43a0853dac64482e31bc
Parthasarathi Singh <pssingh050205@gmail.com>
2024-07-26 01:48:32 +0530

    New React folder

---
+++ React/basic-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Title from './Title'
+
+function App() {
+  return (
+    <div>
+      <Title />
+      <button>Hello World</button>
+    </div>
+  )
+}
+
+export default App
---
+++ React/basic-react-app/src/Title.jsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars, react-refresh/only-export-components
+function Title(){
+    return <h1>Hello World</h1>;
+}
+
+export default Title;
+  
